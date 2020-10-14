7506c707c43b21e7502186a3856b91cbc463e04f
knottyChive <gilyard_jalen@columbusstate.edu>
2020-10-14 15:46:57 -0400

    Finished Test

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const server = express();
 const bodyParser = require('body-parser');
+const axios = require('axios');
+
 
 server.set("view engine", "ejs");
 server.use(bodyParser.urlencoded({extended: true}));
@@ -9,12 +11,48 @@ server.use(express.static(__dirname + '/public'));
 
 const PORT = process.env.PORT || 1234;
 
-server.get('/', (req, res) => {
-    res.render('pages/index');
+
+
+function makeRequest(URLPath) {
+    return new Promise( (resolve, reject) => {
+        axios.get(URLPath)
+        .then(response => {
+            let result = {}
+
+            result.title = response.data.title;
+            result.alt = response.data.alt;
+            result.image = response.data.img;
+            result.month = response.data.month;
+            result.day = response.data.day;
+            result.year = response.data.year;
+
+            
+            resolve(result);
+        })
+        .catch(err => {
+            console.error(err);
+        })
+    })
+
+}
+
+server.get('/', async (req, res) => {
+
+    let comic = await makeRequest('https://xkcd.com/info.0.json');
+    res.render('pages/index', {
+        comic: comic,
+        
+    });
 });
 
-server.get('/random', (req, res) => {
-    res.render('pages/random')
+server.get('/random', async (req, res) => {
+    let num = Math.round(Math.random() * 1001);
+    
+    let randomComic = await makeRequest(`https://xkcd.com/${num}/info.0.json`);
+    res.render('pages/random', {
+        random: randomComic,
+        
+    })
 });
 
 server.listen(PORT, () => {
